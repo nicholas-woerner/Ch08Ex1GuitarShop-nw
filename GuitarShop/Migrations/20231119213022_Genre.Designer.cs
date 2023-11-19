ad2cf745bff3265d274e1408892ba3b626fe5832
Nicholas Woerner <nicholaswoerner@gmail.com>
2023-11-19 17:12:43 -0600

    8-1

--- GuitarShop/Migrations/20231119213022_Genre.Designer.cs
+++ GuitarShop/Migrations/20231119213022_Genre.Designer.cs
@@ -3,6 +3,7 @@ using GuitarShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GuitarShop.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20231119213022_Genre")]
+    partial class Genre
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GuitarShop/Models/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace GuitarShop.Models
+{
+    public class ProductListViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public string SelectedCategory { get; set; }
+        public bool CheckActiveCategory(string category)
+        {
+            return category == SelectedCategory;
+        }
+    }
+}
